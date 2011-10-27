be86a548bfec4bfca8c74592cb7e52b56db8bf06
Ruben Pollan <meskio@sindominio.net>
2011-10-27 17:16:59 +0200

    Events with lock

--- asambleitor/events.py
+++ asambleitor/events.py
@@ -1,4 +1,7 @@
+import thread
+
 _events = {}
+_events_lock = thread.allocate_lock()
 
 def trigger(event, data):
     if event not in _events:
@@ -7,10 +10,14 @@ def trigger(event, data):
         handler(event, data)
 
 def subscribe(event, handler):
+    _events_lock.acquire()
     if event in _events:
         _events[event].append(handler)
     else:
         _events[event] = [handler]
+    _events_lock.release()
 
 def unsubscribe(event, handler):
+    _events_lock.acquire()
     _events[event].remove(handler)
+    _events_lock.release()
