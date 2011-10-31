f5bc512c4eb9b6dd62853b51545695acdec54e8b
Ruben Pollan <meskio@sindominio.net>
2011-10-31 20:02:24 +0100

    Parsing tags on subject

---
+++ emma/interface/email/message.py
@@ -0,0 +1,31 @@
+from email.feedparser import FeedParser
+import email.message
+import re
+
+class Message:
+    def __init__(self, list_lines):
+        p = FeedParser()
+        for line in list_lines:
+            p.feed(line + "\n")
+        self.message = p.close()
+
+        self.lines = list_lines
+        self.comexp = re.compile(r"\[\[([^\|]*)\|([^\]]*)\]\]")
+        self.tagexp = re.compile(r"\[([^\]]*)\]")
+
+    def __getitem__(self, item):
+        if item == 'body':
+            return self.message.get_payload()
+        elif item == 'commands':
+            return self.commands()
+        elif item == 'tags':
+            return self.tags()
+        else:
+            return self.message[item]
+
+    def commands(self):
+        text = self['body']
+        return self.comexp.findall(text)
+
+    def tags(self):
+        return self.tagexp.findall(self['subject'])
